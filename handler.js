1ba2c65146036e4de3f77862a324cbc3d68eb676
Ramakant4168 <ram4168@gmail.com>
2018-11-15 21:46:40 +0530

    Final push

--- handler.js
+++ handler.js
@@ -4,7 +4,7 @@ module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'lambda function executed successfully!',
+      message: 'lambda function executed successfully with circle ci!',
     }),
   };
 
